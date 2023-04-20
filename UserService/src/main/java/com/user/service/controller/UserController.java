501dbc7d00a8df9ac0a701d8248f5d0197813b8d
Meenal Singh <meenal01.0499@gmail.com>
2023-04-20 18:29:21 +0530

    commit

--- UserService/src/main/java/com/user/service/controller/UserController.java
+++ UserService/src/main/java/com/user/service/controller/UserController.java
@@ -4,6 +4,8 @@ import com.user.service.entity.User;
 import com.user.service.services.UserService;
 import com.user.service.services.UserServiceImpl;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +29,15 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
+    int retryCount=1;
     @GetMapping("/{userId}")
-    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    //@CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    //@Retry(name="ratingHotelRetry",fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name="userRateLimiter",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        logger.info("Get Single User Handler: UserController");
+        logger.info("Retry Count: {}", retryCount);
+        retryCount++;
         User user=userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
